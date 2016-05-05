35e99e59c722c7a0936d30273f13a044e15b6612
Ángel Sanz <angelsanzgit@gmail.com>
2016-05-06 01:34:57 +0200

    Replace ternary operator with Enumerable#min

--- lib/bitmap.rb
+++ lib/bitmap.rb
@@ -37,7 +37,7 @@ class Bitmap
     row_start = 0
     width_in_bytes = width / 8
     while row_start < height do
-      chunk_height = ((height - row_start) > 255) ? 255 : (height - row_start)
+      chunk_height = [height - row_start, 255].min
       bytes = (0...(width_in_bytes * chunk_height)).map { @data.getbyte }
       yield chunk_height, width_in_bytes, bytes
       row_start += 255
