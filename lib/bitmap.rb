93de940a61cebf692397f7f442e077712f6c97d1
Ángel Sanz <angelsanzgit@gmail.com>
2016-05-15 20:54:09 +0200

    Bitmap: move responsibility of chunking to BitmapData

--- lib/bitmap.rb
+++ lib/bitmap.rb
@@ -1,66 +1,53 @@
 class Bitmap
-  attr_reader :width
-
   class << self
     def from_source(source)
-      data = DimensionedBitmapData.new(source)
-      new(data.width, data.height, data)
+      new(DimensionedBitmapData.new(source))
     end
 
     def with_dimensions(width, height, source)
-      new(width, height, BitmapData.new(source))
+      new(BitmapData.new(width, height, source))
     end
 
     private :new
   end
 
-  def initialize(width, height, data)
+  def initialize(data)
       @data = data
-      @width = width
-      @height = height
   end
 
   def to_bytes
     bytes = []
-    each_chunk do |height, width, chunk|
+    @data.each_chunk do |height, width, chunk|
       bytes += CHUNK_HEADER + [height, width] + chunk
     end
 
     bytes
   end
 
+  def width
+    @data.width
+  end
 
-  private
+  CHUNK_HEADER = [18, 42]
+  private_constant :CHUNK_HEADER
+end
+
+class BitmapData
+  attr_reader :width
+
+  def initialize(width, height, source)
+    @source = ensure_is_queryable_for_bytes(source)
+    @width = width
+    @height = height
+  end
 
   def each_chunk
     number_of_chunks.times do |chunk_number|
       height_offset = chunk_number * MAXIMUM_CHUNK_HEIGHT
       chunk_height = [@height - height_offset, MAXIMUM_CHUNK_HEIGHT].min
 
-      yield chunk_height, width_in_bytes, @data.get_bytes(width_in_bytes * chunk_height)
+      yield chunk_height, width_in_bytes, get_bytes(width_in_bytes * chunk_height)
     end
-  end
-
-  def number_of_chunks
-    Integer(@height / MAXIMUM_CHUNK_HEIGHT) + 1
-  end
-
-  def width_in_bytes
-    width / 8
-  end
-
-  CHUNK_HEADER = [18, 42]
-  MAXIMUM_CHUNK_HEIGHT = 255
-  private_constant :MAXIMUM_CHUNK_HEIGHT, :CHUNK_HEADER
-end
-
-class BitmapData
-  def initialize(source)
-    @source = ensure_is_queryable_for_bytes(source)
-  end
-
-  def get_bytes(amount)
-    (1..amount).map { @source.getbyte }
   end
 
   private
@@ -69,22 +56,36 @@ class BitmapData
     return source if source.respond_to?(:getbyte)
     StringIO.new(source.map(&:chr).join)
   end
+
+  def number_of_chunks
+    Integer(@height / MAXIMUM_CHUNK_HEIGHT) + 1
+  end
+
+  def width_in_bytes
+    @width / 8
+  end
+
+  def get_bytes(amount)
+    (1..amount).map { @source.getbyte }
+  end
+
+  MAXIMUM_CHUNK_HEIGHT = 255
+  private_constant :MAXIMUM_CHUNK_HEIGHT
 end
 
 class DimensionedBitmapData < BitmapData
-  attr_reader :width, :height
-
   def initialize(source)
-    super(source)
-    @width = read_two_bytes
-    @height = read_two_bytes
+    source = ensure_is_queryable_for_bytes(source)
+    width = read_two_bytes(source)
+    height = read_two_bytes(source)
+
+    super(width, height, source)
   end
 
   private
 
-  def read_two_bytes
-    first_byte = @source.getbyte
-    (@source.getbyte << 8) + first_byte
+  def read_two_bytes(source)
+    first_byte = source.getbyte
+    (source.getbyte << 8) + first_byte
   end
 end
-
